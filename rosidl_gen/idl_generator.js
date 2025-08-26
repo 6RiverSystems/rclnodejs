943b204fb81373fa3114baa14d3b4ff3b8f23f81
Dimitri Nikitopoulos <dnikitopoulos@6river.com>
2025-08-26 08:20:54 -0400

    fix: add check to define message def in the top level message folder (#5)
    * fix: add check to define message def in the top level message folder
    
    * fix: better comment

--- rosidl_gen/idl_generator.js
+++ rosidl_gen/idl_generator.js
@@ -135,10 +135,18 @@ async function generateMessageJSStruct(messageInfo, dir) {
 
 function generateMessageJSStructFromSpec(messageInfo, dir, spec) {
   dir = path.join(dir, `${spec.baseType.pkgName}`);
+  let subFolder = "msg";
+  // Special folders should have the file name point there.
+  // Otherwise, always point to the top level message folder
+  let arr = ["srv", "srvs", "action"];
+  if (arr.includes(messageInfo.subFolder)){
+    subFolder = messageInfo.subFolder;
+  }
+
   const fileName =
     spec.baseType.pkgName +
     '__' +
-    messageInfo.subFolder +
+    subFolder+
     '__' +
     spec.msgName +
     '.js';
